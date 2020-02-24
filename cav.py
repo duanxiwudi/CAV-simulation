4cc439dc5b6fb613ad0cc2b23c6535aa2b06360b
Xi Duan <xiduan@ufl.edu>
2020-02-24 12:33:17 -0500

    calculate seprate speed

--- cav.py
+++ cav.py
@@ -13,6 +13,8 @@ Created on Fri Oct 19 10:25:09 2018
 """
 import win32com.client as com
 import os 
+os.chdir(r"P:\STRIDE on VISSIM CAV\code");
+
 import time
 import pandas as pd
 import auModel
@@ -20,13 +22,13 @@ import auModel
 
 # determine the type of data to be output
 # define the the column names of the output csv file
-out_DataType_Traj=['Time Stamp','Vehicle ID', 'Vehicle Type', 'Vehicle Length','Vehicle Front Coordinate','Vehicle Rear Coordinate','Speed','Acceleration','Headway']
+out_DataType_Traj=['Time Stamp','Vehicle ID', 'Vehicle Type', 'Vehicle Length','Vehicle Front Coordinate','Vehicle Rear Coordinate','Speed','Acceleration','Headway','Lane']
 out_DataType_Signal=['Time Stamp','Signal head ID','Color','Latitude','Longitude','X','Y']
 # the dictionary to change the default output data from vissim to required output data 
 table_vehicle_type = dict({'100':"Passenger Car",'200':"Passenger Truck",'300':"Transit Bus",'400':"Tram",'630':"Passenger Car"})
 table_signal_color = dict({'RED':0, 'AMBER':1, "GREEN":2}) 
 # variables name to fetch the data from vissim
-get_DataType_traj=('No', 'VehType','Length', 'CoordFront','CoordRear','Speed','Acceleration','Hdwy')
+get_DataType_traj=('No', 'VehType','Length', 'CoordFront','CoordRear','Speed','Acceleration','Hdwy', 'Lane' )
 get_DataType_signal=('No','SigState')
 # // specify the vissim version here// 1000 means Vissim 10
 Vissim= com.Dispatch("Vissim.Vissim.1000")
@@ -42,6 +44,25 @@ Layout_file='network2.layx'
 
 # //input your simulation duration here//
 simulation_duration=3600
+
+#// input the lanes number and their corresponding start point's coordinate
+'''
+Lane 4: Eastbound,  (-181, -22)
+Lane 3: Northbound  (-117, -141) 
+Lane 1: Westbound   (60, -1) 
+Lane 2: Southbound  (-165, 131) 
+    
+'''
+eb_coor = (-181, -22)
+nb_coor = (-117, -141)
+wb_coor = (60, -1)
+sb_coor = (165, 131)
+eb_lane = 4
+nb_lane = 3
+wb_lane =1
+sb_lane = 2
+lane_to_cor = {eb_lane : eb_coor, nb_lane :  nb_coor, wb_lane: wb_coor, sb_lane : sb_coor }
+store_speed = pd.DataFrame(columns = ["Vehicle ID","x speed", "y speed"])
 #--------------------------------------------------------------
 ## Load a Vissim Network:
 Filename                = os.path.join(Path_of_COM_Basic_Commands_network,Network_file)
@@ -110,10 +131,10 @@ def leading(lead_vehicle_id,data_set ):
 ##---------------------  setting for the simualtion
 
 #create the dataframe for output
-log_col = ['time','No', 'VehType', 'pos','acceleration',' speed','leading pos', 'leading speed', 'leading acc','leading length','headway','design_acceleration', 'state']
+log_col = ['time','No', 'VehType', 'pos','acceleration',' speed','leading pos', 'leading speed', 'leading acc','leading length','headway','Lane','design_acceleration', 'state']
 log_file = pd.DataFrame(columns = log_col)
 log_file.to_csv(Path_output_file + "log.csv",  index = None,  mode='w')
-dataSet_traj=pd.DataFrame(columns=out_DataType_Traj)
+dataSet_traj = pd.DataFrame(columns=out_DataType_Traj)
 # initiate the simulation parameters
 time_step=0 
 time_sleep=0
@@ -124,10 +145,13 @@ comf_dec = -3.4
 max_acc = 3
 # comfortable acceleration
 comf_acc = 2.5
+pre_data_traj = pd.DataFrame(columns = out_DataType_Traj)
+speed_vehicle = pd.DataFrame(columns = ["Vehicle ID", "Split Speed"])
 
 #-------------------------------------- start simulation
 while time_step < simulation_duration:
-    all_veh_attributes = Vissim.Net.Vehicles.GetMultipleAttributes((get_DataType_traj))              
+    all_veh_attributes = Vissim.Net.Vehicles.GetMultipleAttributes((get_DataType_traj))  
+# select_vehicles,add_data_traj, dataSet_traj  are for emission model purpose            
     select_vehicles=[veh for veh in all_veh_attributes if veh[1]=='630']
 # define and output the signal file here:    
    
@@ -152,41 +176,73 @@ while time_step < simulation_duration:
         add_data_traj.loc[:,'Vehicle Type']=add_data_traj.loc[:,'Vehicle Type'].replace(table_vehicle_type)
         add_data_traj.loc[:,'Time Stamp']=add_data_traj.loc[:,'Time Stamp']/10
         add_data_traj.loc[:,'Acceleration']= add_data_traj.loc[:,'Acceleration']/1.46667 #feet/second /s  to mile/h/ s
+       
+# calculate the lateral and longitudinal speed        
+        
+        speed_cal_curr = add_data_traj.loc[:,["Vehicle ID" , "Vehicle Front Coordinate", "Lane"]]
+        speed_cal_prev = pre_data_traj.loc[:,["Vehicle ID" , "Vehicle Front Coordinate", "Lane"]]        
+        speed_cal_curr["x_coor_curr"] = [float(speed_cal_curr.loc[:, "Vehicle Front Coordinate"][i].split()[0]) for i in range(speed_cal_curr.shape[0]) ]
+        speed_cal_curr["y_coor_curr"] = [float(speed_cal_curr.loc[:, "Vehicle Front Coordinate"][i].split()[1]) for i in range(speed_cal_curr.shape[0]) ]
+        speed_cal_prev["x_coor_prev"] = [float(speed_cal_prev.loc[:, "Vehicle Front Coordinate"][i].split()[0]) for i in range(speed_cal_prev.shape[0]) ]
+        speed_cal_prev["y_coor_prev"] = [float(speed_cal_prev.loc[:, "Vehicle Front Coordinate"][i].split()[1]) for i in range(speed_cal_prev.shape[0]) ]
+        speed_data = pd.merge(speed_cal_curr,speed_cal_prev,   on = "Vehicle ID", how = "outer")
+# check if there are any vehicles just join the network, which does not have prev_coor values
+        if (speed_data.loc[:, "x_coor_prev"].isnull()).any() :
+            for i in speed_data.loc[speed_data.loc[:, "x_coor_prev"].isnull()].index:
+                 lane_num = int(speed_data.loc[i, "Lane_x"].split('-')[0])
+                 speed_data.loc[i, "x_coor_prev"] = lane_to_cor[lane_num][0]
+                 speed_data.loc[i, "y_coor_prev"] = lane_to_cor[lane_num][1]
+        
+        speed_x = speed_data.loc[:, "x_coor_curr"] - speed_data.loc[:, "x_coor_prev"]
+        speed_y = speed_data.loc[:, "y_coor_curr"] - speed_data.loc[:, "y_coor_prev"]
+        pre_data_traj = add_data_traj
+        
+# deque([[VehicleMsg(timestamp=datetime.datetime(2020, 2, 17, 19, 40, 11, 301205), track_id='0', dsrc_id='2000000', pos=[372090.75, 3279903.07], vel=[0.14, -0.75], pos_rms=[0.0, 0.0], vel_rms=[0.0, 0.0], veh_type=0, veh_len=4.57, max_accel=3.05, max_decel=-4.57, road='Default')]], maxlen=1)        
+        
+#----------------------------------------------------------------------------------        
         dataSet_traj=dataSet_traj.append(add_data_traj,ignore_index = True)
         dataSet_traj=dataSet_traj[out_DataType_Traj]                
         dataSet_traj.to_csv(Path_output_file+'Trajectory_data.csv',  index=False )
         
 # run the simulation
         
-        # Method #5: Accessing all attributes directly using "GetMultipleAttributes" (even more faster)
+# Method #5: Accessing all attributes directly using "GetMultipleAttributes" (even more faster)
+# all_veh_attributes are for printing purpose 
         all_veh_attributes = Vissim.Net.Vehicles.GetMultipleAttributes(('No', 'VehType', 'acceleration', 'Speed', 'DistanceToSigHead'))
         for cnt in range(len(all_veh_attributes)):
             print ('%s  |  %s  |  %.2f  |  %.2f  |  %s' % (all_veh_attributes[cnt][0], all_veh_attributes[cnt][1], all_veh_attributes[cnt][2], all_veh_attributes[cnt][3], all_veh_attributes[cnt][4])) # only display the 2nd column)
-        
-        data_vehicles = Vissim.Net.Vehicles.GetMultipleAttributes(('No', 'VehType', 'length','Acceleration','Speed', 'Pos', 'Hdwy','LeadTargNo','LeadTargType','DistanceToSigHead', 'SignalState'))
+## data_vehicles are for AV/CV modeling purpose        
+        data_vehicles = Vissim.Net.Vehicles.GetMultipleAttributes(('No', 'VehType', 'length','Acceleration','Speed', 'Pos', 'Hdwy','LeadTargNo','LeadTargType','DistanceToSigHead', 'SignalState','Lane'))
         dist_stop = (Desired_speed / 3.6) **2 / 2 / -comf_dec
-# 0- No, 1-vehicle type, 2-length, 3-acceleration, 4-speed, 5-position, 6-headway, 7-lead No, 8- lead type, 9-distance to signal, 10- signal state
+#data_vehicles: 0- No, 1-vehicle type, 2-length, 3-acceleration, 4-speed, 5-position, 6-headway, 7-lead No, 8- lead type, 9-distance to signal, 10- signal state, 11- lane
         for vehicle in data_vehicles:
+# it is only used for CAV
             if (vehicle[1] != CAV):
                 continue
+# There are three mode for vehicles, free flow, signal controlled, and car following mode
+# since the AV logic does not consider signal, we designed a signal controlled mode that vehicles are going to decelerate at comfortable deceleration rate when get close enough to signal head.
             dist_safe = (vehicle[4] / 3.6) **2 / 2 / -comf_dec
+            
             if (vehicle[8] == 'VEHICLE' and leading(vehicle[7],data_vehicles)):
                 x_n = vehicle[5]
                 v_n = vehicle[4]
                 l_n_1, a_n_1, v_n_1,x_n_1 = leading(vehicle[7],data_vehicles) 
                 ak = auModel.AV_Model( x_n, v_n / 3.6, x_n_1, v_n_1 /3.6, a_n_1, l_n_1).cal_acc() 
-                if v_n_1 == 0 and vehicle[6] < dist_safe + 5:
+                if v_n_1 < 5 and vehicle[6] < dist_safe + 5:
                     ak = comf_dec
-                record = [[time_step, vehicle[0],vehicle[1], x_n,vehicle[3], v_n / 3.6, x_n_1, v_n_1 /3.6, a_n_1, l_n_1,x_n_1 - x_n, ak, 'car following']]
-            elif vehicle[9] <dist_safe + 5 and vehicle[10] =='RED':
+                if vehicle[9] < dist_safe + 5 and (vehicle[10] =='RED' or vehicle[10] =='AMBER'):    
+                    ak = comf_dec
+                record = [[time_step, vehicle[0],vehicle[1], x_n,vehicle[3], v_n / 3.6, x_n_1, v_n_1 /3.6, a_n_1, l_n_1,x_n_1 - x_n,vehicle[11], ak, 'car following']]
+            elif vehicle[9] < dist_safe + 5 and (vehicle[10] =='RED' or vehicle[10] =='AMBER'):    
                 ak = comf_dec;
-                record = [[time_step, vehicle[0],vehicle[1], vehicle[5],vehicle[3], vehicle[4] / 3.6,vehicle[5] + vehicle[9] , 0 , 0 ,0,vehicle[9], ak, 'signal']]
+                record = [[time_step, vehicle[0],vehicle[1], vehicle[5],vehicle[3], vehicle[4] / 3.6,vehicle[5] + vehicle[9] , 0 , 0 ,0,vehicle[9], vehicle[11], ak, 'signal']]
+            
             else:
                 ak = min(comf_acc, (Desired_speed  - vehicle[4]) / 3.6 *0.58 if vehicle[4] < Desired_speed else 0 )
-                record = [[time_step, vehicle[0],vehicle[1], vehicle[5],vehicle[3], vehicle[4] / 3.6,0 , 0 ,0, 0 ,0 , ak, 'free']]
+                record = [[time_step, vehicle[0],vehicle[1], vehicle[5],vehicle[3], vehicle[4] / 3.6,0 , 0 ,0, 0 ,0 ,vehicle[11], ak, 'free']]
             log_file = log_file.append(pd.DataFrame(record,  columns = log_col),ignore_index=True)
             vehicle_ID.append(record[0][1])
-            vehicle_acc.append(record[0][11])  
+            vehicle_acc.append(record[0][12])  
         time.sleep(time_sleep)
         ## update the traj file, here plus 0.01 
         traj = pd.DataFrame({'a':vehicle_ID,'b':vehicle_acc})
