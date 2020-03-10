3f677bb3de3364ea613960a328fbad87c7bcfa0a
Xi Duan <xiduan@ufl.edu>
2020-03-10 11:45:16 -0400

    add some parameters control

--- cav simulation.py
+++ cav simulation.py
@@ -15,10 +15,49 @@ from data_io import msgManager
 import time
 import pandas as pd
 import auModel
+import argparse
+
+#--
 # os.chdir(r"C:\Users\essie-adm-luan\Downloads\CAV-simulation-xixi\CAV-simulation-xixi");
 # LC: saves you the effort of updating the working directory
 currentDir = os.path.abspath(os.path.dirname(__file__)) 
 os.chdir(currentDir)
+#---
+
+parser = argparse.ArgumentParser()
+
+# directory setting
+parser.add_argument('--path_output_file', default = os.path.join(currentDir,"output"), help = "the directory of output file, the default is the current working directory ")
+parser.add_argument('--path_of_network', default = os.path.join(currentDir, "network"), help = "the directory of network file, including .inpx and .layx")
+
+# simulation parameters setting
+parser.add_argument('--version_VISSIM', default = "Vissim.Vissim.1000" , help = "1000 means Vissim 10")
+parser.add_argument('--network_file', default = "network2.inpx" , help = "the name of VISSIM network file")
+parser.add_argument('--layout_file', default = "network2.layx" , help = "the name of VISSIM layout file")
+
+parser.add_argument('--duration', default = 3600 ,type = int, help = "simulation duration, unit in second")
+parser.add_argument('--time_sleep', default = 0 ,type = float, help = "the sleep time between each time step of simulation")
+# simulation traffic input setting 
+parser.add_argument('--sb_volume', default = 1500 ,type = int, help = "traffic volume of southbound, vehicles / hr")
+parser.add_argument('--nb_volume', default = 1500 ,type = int, help = "traffic volume of northbound, vehicles / hr")
+parser.add_argument('--wb_volume', default = 1500 ,type = int, help = "traffic volume of westbound, vehicles / hr")
+parser.add_argument('--eb_volume', default = 1500 ,type = int, help = "traffic volume of eastbound, vehicles / hr")
+parser.add_argument('--CAV_rel_flow', default = 0.2 ,type = float, help = "the percentage of CAV volume")
+
+
+
+# traffic model setting
+parser.add_argument('--max_dec', default = -5.0 ,type = float, help = "the maximum deceleration, m / s^2")
+parser.add_argument('--comf_dec', default = -3.5 ,type = float, help = "the comfortable deceleration, m / s^2")
+parser.add_argument('--max_acc', default = 3 ,type = float, help = "the maximum acceleration, m / s^2")
+parser.add_argument('--comf_acc', default = 2.5 ,type = float, help = "the comfortable acceleration, m / s^2")
+
+simulation = parser.parse_args()
+
+
+
+
+
 
 # Loads the message manager that will send and receive the messages
 msgCfgFile = os.path.join(currentDir, "data_io_config", "Local_RIO_Vissim.yml")
@@ -30,11 +69,15 @@ sendMessage = False # this is to avoid breaking the code for now.
 
 
 
-# determine the type of data to be output
+# determine the type of data to be output --------------------------------------
 # define the the column names of the output csv file
-out_DataType_Traj=['Time Stamp','Vehicle ID', 'Vehicle Type', 'Vehicle Length','Vehicle Front Coordinate','Vehicle Rear Coordinate','Speed','Acceleration','Headway','Lane']
-out_DataType_Signal=['Time Stamp','Signal head ID','Color','Latitude','Longitude','X','Y']
-out_DataType_Traj_em=['Time Stamp','Vehicle ID', 'Vehicle Type', 'Vehicle Length','Vehicle Front Coordinate','Vehicle Rear Coordinate','Speed','Acceleration','Headway']
+
+out_DataType_Traj = ['Time Stamp','Vehicle ID', 'Vehicle Type', 'Vehicle Length','Vehicle Front Coordinate','Vehicle Rear Coordinate','Speed','Acceleration','Headway','Lane']
+# data type for signal output
+out_DataType_Signal = ['Time Stamp','Signal head ID','Color','Latitude','Longitude','X','Y']
+# data type for trajectory output
+out_DataType_Traj_em = ['Time Stamp','Vehicle ID', 'Vehicle Type', 'Vehicle Length','Vehicle Front Coordinate','Vehicle Rear Coordinate','Speed','Acceleration','Headway']
+# data type for CV (RIO) output
 out_DataType_CVModel = ["No", "Pos", "Speed","pos_rms", "speed_rms", "veh_type", "veh_length", "max_accel", "max_decel"]
 # the dictionary to change the default output data from vissim to required output data 
 table_vehicle_type = dict({'100':"Passenger Car",'200':"Passenger Truck",'300':"Transit Bus",'400':"Tram",'630':"Passenger Car"})
@@ -43,21 +86,7 @@ table_signal_color = dict({'RED':0, 'AMBER':1, "GREEN":2})
 get_DataType_traj=('No', 'VehType','Length', 'CoordFront','CoordRear','Speed','Acceleration','Hdwy', 'Lane' )
 get_DataType_signal=('No','SigState')
 # // specify the vissim version here// 1000 means Vissim 10
-Vissim= com.Dispatch("Vissim.Vissim.1000")
-
-#User input---------------------------------------------------- 
-# //input your fold directory which contains the Vissim file here//
-system_dr = os.getcwd()
-
-Path_of_COM_Basic_Commands_network =  os.path.join(system_dr, "network")
-# // write your output fold directory here//
-Path_output_file = os.path.join(system_dr, "output")
-# //input your network file and layout file name here//
-Network_file='network2.inpx'
-Layout_file='network2.layx'
-
-# //input your simulation duration here//
-simulation_duration=3600
+Vissim= com.Dispatch(simulation.version_VISSIM)
 
 #// input the lanes number and their corresponding start point's coordinate
 '''
@@ -71,8 +100,8 @@ eb_coor = (-181, -22)
 nb_coor = (-117, -141)
 wb_coor = (60, -1)
 sb_coor = (165, 131)
-eb_lane = 4
-nb_lane = 3
+eb_lane = 3
+nb_lane = 4
 wb_lane =1
 sb_lane = 2
 lane_to_cor = {eb_lane : eb_coor, nb_lane :  nb_coor, wb_lane: wb_coor, sb_lane : sb_coor }
@@ -81,31 +110,33 @@ vehicle_type_dict = {"Passenger Car" : 0}
 
 #--------------------------------------------------------------
 ## Load a Vissim Network:
-Filename                = os.path.join(Path_of_COM_Basic_Commands_network,Network_file)
+Filename                = os.path.join(simulation.path_of_network,simulation.network_file)
 flag_read_additionally  = False # you can read network(elements) additionally, in this case set "flag_read_additionally" to true
 Vissim.LoadNet(Filename, flag_read_additionally)
 ## Load a Layout:
-Filename = os.path.join(Path_of_COM_Basic_Commands_network, Layout_file)
+Filename = os.path.join(simulation.path_of_network,simulation.layout_file)
 Vissim.LoadLayout(Filename)
 CAV = '630'
+TRA_car = '100'
 # Set vehicle input:
 # // change the vehicles input here//
-VI_number   = 1 # VI = Vehicle Input
-new_volume  = 1500 # vehicles per hour
-Vissim.Net.VehicleInputs.ItemByKey(VI_number).SetAttValue('Volume(1)', new_volume)
-
+Desired_speed = 70
+cav_flow_rate = simulation.CAV_rel_flow
+Vissim.Net.VehicleInputs.ItemByKey(1).SetAttValue('Volume(1)', simulation.eb_volume)
+Vissim.Net.VehicleInputs.ItemByKey(2).SetAttValue('Volume(1)', simulation.nb_volume)
+Vissim.Net.VehicleInputs.ItemByKey(3).SetAttValue('Volume(1)', simulation.wb_volume)
+Vissim.Net.VehicleInputs.ItemByKey(4).SetAttValue('Volume(1)', simulation.sb_volume)
 # Set vehicle composition:
 # //set the vehicles composition here//
 Veh_composition_number = 1
-Desired_speed = 70
 Rel_Flows = Vissim.Net.VehicleCompositions.ItemByKey(Veh_composition_number).VehCompRelFlows.GetAll()
 # here the type 630 is the connected vehicles,type 100 is the normal vehicles
-Rel_Flows[0].SetAttValue('VehType',        100) # Changing the vehicle type
+Rel_Flows[0].SetAttValue('VehType',        TRA_car) # Changing the vehicle type
 Rel_Flows[1].SetAttValue('VehType',        CAV) # Changing the vehicle type
 Rel_Flows[0].SetAttValue('DesSpeedDistr',   Desired_speed) # Changing the desired speed distribution
 Rel_Flows[1].SetAttValue('DesSpeedDistr',   Desired_speed) # Changing the desired speed distribution
-Rel_Flows[0].SetAttValue('RelFlow',        50) # Changing the relative flow
-Rel_Flows[1].SetAttValue('RelFlow',        50) # Changing the relative flow of the 2nd Relative Flow.
+Rel_Flows[0].SetAttValue('RelFlow',        (1- cav_flow_rate) * 100) # Changing the relative flow
+Rel_Flows[1].SetAttValue('RelFlow',        cav_flow_rate *100) # Changing the relative flow of the 2nd Relative Flow.
 
 ## function to calculate the distance
 # a, b are two list contain coordinate like [x,y,z]
@@ -148,28 +179,20 @@ def leading(lead_vehicle_id,data_set ):
 log_col = ['time','No', 'VehType', 'pos','acceleration',' speed','leading pos', 'leading speed', 'leading acc','leading length','headway','Lane','design_acceleration', 'state']
 log_file = pd.DataFrame(columns = log_col)
 CV_data = pd.DataFrame(columns = out_DataType_CVModel)
-log_file.to_csv(Path_output_file + "\\log.csv",  index = None,  mode='w')
+log_file.to_csv(simulation.path_output_file + "\\log.csv",  index = None,  mode='w')
 dataSet_traj = pd.DataFrame(columns=out_DataType_Traj)
 dataSet_signal=pd.DataFrame(columns=out_DataType_Signal)
 # initiate the simulation parameters
 time_step=0 
-time_sleep=0
-simulation_duration=3600
-max_dec = -5
-# comfortable deceleration
-comf_dec = -3.4
-max_acc = 3
-# comfortable acceleration
-comf_acc = 2.5
 pre_data_traj = pd.DataFrame(columns = out_DataType_Traj)
 speed_vehicle = pd.DataFrame(columns = ["Vehicle ID", "Split Speed"])
 ## Read the signal coordinate data:
-signal_input = pd.read_excel(Path_output_file+"\\Signal data input.xlsx")
+signal_input = pd.read_excel(simulation.path_output_file + "\\Signal data input.xlsx")
 #-------------------------------------- start simulation
 #  Random_Seed = 42
 #  Vissim.Simulation.SetAttValue('RandSeed', Random_Seed)
 #
-while time_step < simulation_duration:
+while time_step < simulation.duration:
     all_veh_attributes = Vissim.Net.Vehicles.GetMultipleAttributes((get_DataType_traj))  
 # select_vehicles,add_data_traj, dataSet_traj  are for emission model purpose            
     all_veh_attributes=[veh for veh in all_veh_attributes]
@@ -185,12 +208,12 @@ while time_step < simulation_duration:
 #output
     dataSet_signal=dataSet_signal.append(add_data_signal,ignore_index = True)
     dataSet_signal=dataSet_signal[out_DataType_Signal]                
-    dataSet_signal.to_csv(Path_output_file+'\\Signal_data.csv',  index=False )    
+    dataSet_signal.to_csv(simulation.path_output_file +'\\Signal_data.csv',  index=False )    
 #output trajectory data here
 # check if have the CVs in the network
     if not all_veh_attributes:
         time_step+=1
-        time.sleep(time_sleep)
+        time.sleep(simulation.time_sleep)
         Vissim.Simulation.RunSingleStep()
     else:
 # collect and output the trajectory data
@@ -229,16 +252,15 @@ while time_step < simulation_duration:
         speed_coor = [[speed_x[i], speed_y[i]] for i in range(len(speed_x))]
         add_data_traj["speed_coor"] = speed_coor
         CV_data = add_data_traj.loc[:, ["Vehicle ID", "Vehicle Front Coordinate","speed_coor", "nan", "nan", "Vehicle Type", "Vehicle Length"] ]
-        CV_data["max_acc"] = max_acc
-        CV_data["max_decce"] = max_dec
+        CV_data["max_acc"] = simulation.max_acc
+        CV_data["max_decce"] = simulation.max_dec
         CV_data["Vehicle Front Coordinate"] = [ i.split()[0:2] for i in (CV_data.loc[:, "Vehicle Front Coordinate"])]
         CV_data.replace(vehicle_type_dict, inplace = True)
         pre_data_traj = add_data_traj
     
 #----------------------------------------------------------------------------------        
-        dataSet_traj=dataSet_traj.append(add_data_traj,ignore_index = True)
-        dataSet_traj=dataSet_traj[out_DataType_Traj_em]                
-        dataSet_traj.to_csv(Path_output_file+'\\Trajectory_data.csv',  index=False )
+        add_data_traj = add_data_traj[out_DataType_Traj_em]
+        add_data_traj.to_csv(simulation.path_output_file + '\\Trajectory_data.csv',  header = None,  index = None,  mode='a' )
         
 # run the simulation
         
@@ -249,7 +271,7 @@ while time_step < simulation_duration:
             print ('%s  |  %s  |  %.2f  |  %.2f  |  %s' % (all_veh_attributes[cnt][0], all_veh_attributes[cnt][1], all_veh_attributes[cnt][2], all_veh_attributes[cnt][3], all_veh_attributes[cnt][4])) # only display the 2nd column)
 ## data_vehicles are for AV/CV modeling purpose        
         data_vehicles = Vissim.Net.Vehicles.GetMultipleAttributes(('No', 'VehType', 'length','Acceleration','Speed', 'Pos', 'Hdwy','LeadTargNo','LeadTargType','DistanceToSigHead', 'SignalState','Lane'))
-        dist_stop = (Desired_speed / 3.6) **2 / 2 / -comf_dec
+        dist_stop = (Desired_speed / 3.6) **2 / 2 / -(simulation.comf_dec)
 #data_vehicles: 0- No, 1-vehicle type, 2-length, 3-acceleration, 4-speed, 5-position, 6-headway, 7-lead No, 8- lead type, 9-distance to signal, 10- signal state, 11- lane
         for vehicle in data_vehicles:
 # it is only used for CAV
@@ -257,7 +279,7 @@ while time_step < simulation_duration:
                 continue
 # There are three mode for vehicles, free flow, signal controlled, and car following mode
 # since the AV logic does not consider signal, we designed a signal controlled mode that vehicles are going to decelerate at comfortable deceleration rate when get close enough to signal head.
-            dist_safe = (vehicle[4] / 3.6) **2 / 2 / -comf_dec
+            dist_safe = (vehicle[4] / 3.6) **2 / 2 /  -(simulation.comf_dec)
             
             if (vehicle[8] == 'VEHICLE' and leading(vehicle[7],data_vehicles)):
                 x_n = vehicle[5]
@@ -265,25 +287,25 @@ while time_step < simulation_duration:
                 l_n_1, a_n_1, v_n_1,x_n_1 = leading(vehicle[7],data_vehicles) 
                 ak = auModel.AV_Model( x_n, v_n / 3.6, x_n_1, v_n_1 /3.6, a_n_1, l_n_1).cal_acc() 
                 if v_n_1 < 5 and vehicle[6] < dist_safe + 5:
-                    ak = comf_dec
+                    ak = simulation.comf_dec
                 if vehicle[9] < dist_safe + 5 and (vehicle[10] =='RED' or vehicle[10] =='AMBER'):    
-                    ak = comf_dec
+                    ak = simulation.comf_dec
                 record = [[time_step, vehicle[0],vehicle[1], x_n,vehicle[3], v_n / 3.6, x_n_1, v_n_1 /3.6, a_n_1, l_n_1,x_n_1 - x_n,vehicle[11], ak, 'car following']]
             elif vehicle[9] < dist_safe + 5 and (vehicle[10] =='RED' or vehicle[10] =='AMBER'):    
-                ak = comf_dec;
+                ak = simulation.comf_dec;
                 record = [[time_step, vehicle[0],vehicle[1], vehicle[5],vehicle[3], vehicle[4] / 3.6,vehicle[5] + vehicle[9] , 0 , 0 ,0,vehicle[9], vehicle[11], ak, 'signal']]
             
             else:
-                ak = min(comf_acc, (Desired_speed  - vehicle[4]) / 3.6 *0.58 if vehicle[4] < Desired_speed else 0 )
+                ak = min(simulation.comf_acc, (Desired_speed  - vehicle[4]) / 3.6 *0.58 if vehicle[4] < Desired_speed else 0 )
                 record = [[time_step, vehicle[0],vehicle[1], vehicle[5],vehicle[3], vehicle[4] / 3.6,0 , 0 ,0, 0 ,0 ,vehicle[11], ak, 'free']]
             log_file = log_file.append(pd.DataFrame(record,  columns = log_col),ignore_index=True)
             vehicle_ID.append(record[0][1])
             vehicle_acc.append(record[0][12])  
-        time.sleep(time_sleep)
+        time.sleep(simulation.time_sleep)
         ## update the traj file, here plus 0.01 
         traj = pd.DataFrame({'a':vehicle_ID,'b':vehicle_acc})
-        traj.to_csv(Path_output_file + "\\Trajectory.txt", header=None, index = None, sep=' ', mode='w')
-        log_file.to_csv(Path_output_file + "\\log.csv", header = None,  index = None,  mode='a')
+        traj.to_csv(simulation.path_output_file + "\\Trajectory.txt", header=None, index = None, sep=' ', mode='w')
+        log_file.to_csv(simulation.path_output_file + "\\log.csv", header = None,  index = None,  mode='a')
        
         
 
